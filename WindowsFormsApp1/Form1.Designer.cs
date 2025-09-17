fa66254c11cb748fb36bca3f47aa5e46455f3059
a114221096-wq <a114221096@mail.shu.edu.tw>
2025-09-17 13:09:53 +0800

    114_09_17

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.message = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // message
+            // 
+            this.message.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.message.Location = new System.Drawing.Point(286, 238);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(311, 99);
+            this.message.TabIndex = 0;
+            this.message.Text = "顯示訊息";
+            this.message.UseVisualStyleBackColor = true;
+            this.message.Click += new System.EventHandler(this.message_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.message);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button message;
+    }
+}
+
