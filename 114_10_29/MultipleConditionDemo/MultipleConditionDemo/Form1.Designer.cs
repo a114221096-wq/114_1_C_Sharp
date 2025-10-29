823284b9ceb322eda77385d8c929ca8c9e6a3321
a114221096-wq <a114221096@mail.shu.edu.tw>
2025-10-29 13:01:27 +0800

    114_10_29

---
+++ 114_10_29/MultipleConditionDemo/MultipleConditionDemo/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿        namespace MultipleConditionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnJudge;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IblGrade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblScore.Location = new System.Drawing.Point(59, 77);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(121, 47);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "成績";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(186, 86);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(115, 29);
+            this.txtScore.TabIndex = 1;
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(136, 351);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(176, 63);
+            this.btnJudge.TabIndex = 2;
+            this.btnJudge.Text = "等級判斷";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.btnJudge_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(59, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 47);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "等級";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // IblGrade
+            // 
+            this.IblGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.IblGrade.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.IblGrade.Location = new System.Drawing.Point(186, 174);
+            this.IblGrade.Name = "IblGrade";
+            this.IblGrade.Size = new System.Drawing.Size(249, 46);
+            this.IblGrade.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.IblGrade);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label IblGrade;
+    }
+}
+
---
+++ 114_10_29/MultipleConditionDemo/MultipleConditionDemo/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleConditionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJudge_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Score = int.Parse(txtScore.Text);
+                string grade;
+                if (Score >= 60)
+                {
+                    if (Score >= 70)
+                    {
+                        if (Score >= 80)
+                        {
+                            if (Score >= 90)
+                            {
+                                grade = "A";
+                            }
+                            else
+                            {
+                                grade = "B";
+                            }
+                        }
+                        else
+                        {
+                            grade = "C";
+                        }
+                    }
+                    else
+                    {
+                        grade = "D";
+                    }
+                }
+                else
+                {
+                    grade = "F";
+                }
+                IblGrade.Text = grade;
+                MessageBox.Show($"成績等級為:{grade}");
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("請輸入有效整數成績", "輸入錯誤", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                IblGrade.Text = "";
+                return;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
