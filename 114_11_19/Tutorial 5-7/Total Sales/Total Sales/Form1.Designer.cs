a46571e0e68feebf40e47f23af31855d983d793a
a114221096-wq <a114221096@mail.shu.edu.tw>
2025-11-19 13:49:23 +0800

    114_11_19

---
+++ 114_11_19/Tutorial 5-7/Total Sales/Total Sales/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Total_Sales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saslesListBox = new System.Windows.Forms.ListBox();
+            this.outputDescriptionLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saslesListBox
+            // 
+            this.saslesListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saslesListBox.FormattingEnabled = true;
+            this.saslesListBox.ItemHeight = 40;
+            this.saslesListBox.Location = new System.Drawing.Point(56, 74);
+            this.saslesListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.saslesListBox.Name = "saslesListBox";
+            this.saslesListBox.Size = new System.Drawing.Size(384, 324);
+            this.saslesListBox.TabIndex = 0;
+            // 
+            // outputDescriptionLabel
+            // 
+            this.outputDescriptionLabel.AutoSize = true;
+            this.outputDescriptionLabel.Location = new System.Drawing.Point(42, 32);
+            this.outputDescriptionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputDescriptionLabel.Name = "outputDescriptionLabel";
+            this.outputDescriptionLabel.Size = new System.Drawing.Size(85, 18);
+            this.outputDescriptionLabel.TabIndex = 1;
+            this.outputDescriptionLabel.Text = "總銷售額:";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalLabel.Location = new System.Drawing.Point(161, 21);
+            this.totalLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(119, 41);
+            this.totalLabel.TabIndex = 2;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(56, 419);
+            this.calculateButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(224, 62);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "讀取銷售數據並計算總額";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(299, 419);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(224, 62);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "退出";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 509);
+            this.Controls.Add(this.saslesListBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.outputDescriptionLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "總銷售額";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // 新增 ListBox 欄位，用來在介面上顯示讀取到的每一筆銷售資料
+        private System.Windows.Forms.ListBox saslesListBox;
+        private System.Windows.Forms.Label outputDescriptionLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 114_11_19/Tutorial 5-7/Total Sales/Total Sales/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Total_Sales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+            decimal totalSale = 0.0m;
+            decimal currentSale = 0.0m;
+            string line;
+            try
+            {
+                inputFile = File.OpenText("Sales.txt");
+                saslesListBox.Items.Clear();
+
+                while (!inputFile.EndOfStream)
+                {
+                    if (decimal.TryParse(line = inputFile.ReadLine(), out currentSale))
+                    {
+                        saslesListBox.Items.Add(line);
+                        totalSale += currentSale;
+                    }
+                    else
+                    {
+                        MessageBox.Show("無法解析銷售額:" + line);
+                        break;
+                    }
+                }
+                inputFile.Close();
+                totalLabel.Text = totalSale.ToString("C");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("發生錯誤" + ex.Message);
+                return;
+            }
+            }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
